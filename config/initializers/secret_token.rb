2b8646987ea237f2f0fc20655209db19a3fa0248
Haben Amare <habenweb@gmail.com>
2014-07-29 04:47:59 +0300

    Initial commit
    Group mysql gem under development to prevent confilicts on heroku
    
    Change .gitignore file to add more file types to be ignored

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mysanjo::Application.config.secret_key_base = '35a2480aca1f942131a9b4c317bea15a24488faffc0fe7ce7b6e1f564429f751d855643baa5588a9b63d69c5e77f9fb5a75081029b591baa88d794613f56693f'
